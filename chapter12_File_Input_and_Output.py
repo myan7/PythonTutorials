4b4c3aff46b070e7106ad37ab799d8dd80f7462f
Ming Yan <myan7@hawk.iit.edu>
2021-12-15 07:19:56 +0000

    chapter 12 ongoing

--- chapter12_File_Input_and_Output.py
+++ chapter12_File_Input_and_Output.py
@@ -199,6 +199,8 @@ relativePath.is_file() {relativePath.is_file()}
 
 print(f"""{sec}
 12.3 Common File System Operations
+you will learn how to
+create/move a file or directory
 """)
 
 print(f"""{seg}
@@ -270,6 +272,7 @@ touch(self, mode=438, exist_ok=True)
     you need to create the parents directory first.
     also exist_ok is default to True, so no need to worry if you will
     overwrite the file.
+    see review exercise as an example.
 """)
 
 file = nestedPath/"test_a_b.txt"
@@ -418,4 +421,401 @@ for f in list_relativePath_parent2_glob2:
 
 print(f"""{seg}
 Moving and Deleting Files and Folders
+    1.To move a file, use the .replace() method.
+""")
+print(r"""
+>>> help(Path.replace)
+Help on function replace in module pathlib:
+
+replace(self, target)
+    Rename this path to the target path, overwriting if that path exists.
+    
+    The target path may be absolute or relative. Relative paths are
+    interpreted relative to the current working directory, *not* the
+    directory of the Path object.
+    
+    Returns the new Path instance pointing to the target path.
+""")
+currPath = Path.cwd()
+chapter12 = currPath/"chapter12_Exercise"
+chapter12.mkdir(exist_ok=True)
+
+## check what files are in this directory
+chapter12_glob = list(chapter12.glob("*"))
+origin = ""
+for f in chapter12_glob:
+    tempPath = chapter12
+    if f.is_dir():
+        print(f"Dictory:{f}")
+    if f.is_file():
+        print(f"File: {f}")
+        if "test" in f.__str__():
+            origin = f
+print(r"""
+1st time run:
+Dictory:E:\pythonProject\PythonTutorials\chapter12_Exercise\folder_A
+File: E:\pythonProject\PythonTutorials\chapter12_Exercise\test.txt
+E:\pythonProject\PythonTutorials\chapter12_Exercise\test.txt has been moved to
+E:\pythonProject\PythonTutorials\chapter12_Exercise\folder_A\destination_of_replace.txt
+""")
+
+print(r"""
+2nd time run:
+E:\pythonProject\PythonTutorials\chapter12_Exercise\folder_A\destination_of_replace.txt already exists.
+are you sure you want to replace?Y
+Traceback (most recent call last):
+  File "E:\pythonProject\PythonTutorials\chapter12_File_Input_and_Output.py", line 469, in <module>
+    origin.replace(destination)
+TypeError: replace expected at least 2 arguments, got 1
+This is because the source file has been moved to destination already.
+there is no source file, origin in the directory.
+origin is now "".
+""")
+
+print(r"""
+3rd time run:
+after I added a file check for origin:
+origin file is missing, could be moved already.
+You can always create a file with "test" in directory chapter12_Exercise,
+and play with it.
+""")
+
+destination = chapter12/"folder_A"/"destination_of_replace.txt"
+
+if origin != "":
+    if not destination.exists():
+        origin.replace(destination)
+        print(f"""{origin}
+    has been moved to
+    {destination}""")
+    else:
+        message = input(f"""{destination} already exists.
+    are you sure you want to replace?""")
+        if message.title() == 'Y':
+            origin.replace(destination)
+else:
+    print("origin file is missing, could be moved already.")
+
+
+
+print(f"""
+If the destination path already exists, .replace() overwrites the
+destination with the source file without raising any kind of exception.
+This can cause undesired loss of data if you aren’t careful.
+You may want to first check if the destination file exists, and
+move the file only in the case that it does not:
+if not destination.exists():
+    source.replace(destination)
+
+also you may want to check if the source exists too!
+{seg}
+Moving and Deleting Files and Folders
+    2.To move a directory, use the .replace() method.
+""")
+
+
+print(f"""{seg}
+Moving and Deleting Files and Folders
+    3.To remove a file, use the .unlink() method.
+    If the path that you call .unlink() does not exists, a FileNotFoundError
+    exception is raised:
+        FileNotFoundError:The system cannot find the file specified:
+    If you want to ignore the exception,
+    set the optional missing_ok parameter to True:
+        file_path.unlink(missing_ok=True)
+    In this case, nothing actually happens because the file located at
+    file_path does not exist.
+""")
+
+
+print(f"""{seg}
+Moving and Deleting Files and Folders
+    4.To remove a directory, use the .rmdir() method.
+    .unlink() only works for paths representing files.
+    To remove a directory, use the .rmdir() method. 
+""")
+
+
+print(f"""{seg}
+Moving and Deleting Files and Folders
+    5.To remove a directory with sub directories,
+    use the rmtree() method from the built-in shutil module.
+""")
+
+print(f"""{sec}
+12.5 Reading and Writing Files
+{sec}
+""")
+
+
+print(f"""{seg}
+Understanding Text Files
+{seg}
+refer to encoding
+https://realpython.com/python-encodings-guide/#enter-unicode
+""")
+
+
+print(f"""{seg}
+Python File Objects
+{seg}
+1. open a file
+{seg}
+
+built-in open() vs Path.open()
+according to io's documentation:
+io.open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None, closefd=True, opener=None)
+This is an alias for the builtin open() function.
+https://docs.python.org/3/library/io.html#io.open
+
+pathlib.Path.open is just a wrapper for the built-in open function.
+{seg}
+The with Statement
+{seg}
+To ensure that file system resources are cleaned up even if a program
+crashes, you can open a file in a with statement.
+this is called context manager.
+Context managers allow you to allocate and release resources precisely when you want to.
+The most widely used example of context managers is the with statement.
+
+refer to:
+https://book.pythontips.com/en/latest/context_managers.html
+
+
+""")
+
+print(f"""{seg}
+Python File Objects
+{seg}
+2. Reading data from a file
+{seg}
+The .read() method reads all of the text in the file and returns it as a
+string value
+
+The .readlines() method returns an iterable of lines from the file.
+""")
+
+print(f"\nprint the destination file directly and then it's name:")
+with open(destination,"r") as f:
+        print(f"destination is \n{f}")
+        print(f"destination.name is \n{f.name}")
+
+
+print(f"\nprint the file by looping thru it:")
+with open(destination,encoding='utf8') as f:
+#    print(f"destination is {f}")
+    print()
+    if len(f.readlines()) > 0:
+        print(f"yes, length is {len(f.readlines()) }")
+        for c in f.readlines():
+            print(f"content of destintion is {c}")
+    else:
+        print(f"length of destination is len(f.readlines())")
+
+print(f"\nread the file first and then check the type and length.")
+with open(destination,"r",encoding='UTF-8') as f:
+    text = f.readlines()
+    print(f"type of text is {type(text)}")
+    print(f"length of text is {len(text)}")          # why this works？
+    print(f"length of f.readlines() is {len(f.readlines())}") # why this doesn't work?
+    text2 = f.read()
+    print(f"type of text2 is {type(text2)}")
+    print(f"test2 is {text2}")
+    print(f"length of text2 is {len(text2)}")          
+    print(f"length of f.read() is {len(f.read())}") 
+    
+    print(f"content of destintion is:")
+    lineNum = 1
+    for c in text:
+        print(f"{lineNum}: {c}",end='')
+        lineNum += 1
+print(r"""
+read(size=-1, /) method of _io.TextIOWrapper instance
+    Read at most n characters from stream.
+    
+    Read from underlying buffer until we have n characters or we hit EOF.
+    If n is negative or omitted, read until EOF.
+
+>>> help(f.readlines)
+Help on built-in function readlines:
+
+readlines(hint=-1, /) method of _io.TextIOWrapper instance
+    Return a list of lines from the stream.
+    
+    hint can be specified to control the number of lines read: no more
+    lines will be read if the total size (in bytes/characters) of all
+    lines so far exceeds hint.
+""")
+
+
+destination2 = chapter12/"folder_A"/"destination_of_replace2.txt"
+
+#destination2.touch()
+
+with open(destination2,"w") as f:
+    f.write("Hello\nPython\nMy name is Ming")
+
+with open(destination2,"r") as f:
+    text = f.readlines()
+    print(f"""
+destination2 is {destination2};
+destination2 type is {type(destination2)};
+length of f.readlines() is {len(f.readlines())};
+
+# you have to store f.readlines() to a variable
+text = f.readlines()
+
+text is {text};
+text type is {type(text)}
+length of text is {len(text)};
+""")
+    for c in text:
+        print(c,end='')
+
+
+print(f"""
+{seg}
+Python File Objects
+{seg}
+3. Writing Data To a File
+{seg}
+
+To write data to a plain text file, you pass a string to a file object’s
+.write() method. The file object must be opened in write mode by
+passing the value "w" to the mode parameter.
+
+You can append data to the end of a file by opening the file in append
+mode by passing the value "a" to the mode parameter.
+
+You can write multiple lines to a file at the same time using the
+.writelines() method. First, create a list of strings:
+Then open the file in write mode and use the .writelines() method to
+write each string in the list to the file:
+""")
+
+
+print(f"""{seg}
+Python File Objects
+{seg}
+Review Exercises
+{seg}
+""")
+
+revEx_12 = chapter12/"ReviewExercise"/"chapter12_reviewExercise"/"revEx.txt"
+# revEx_12 points to a new file that hasn't been created, along with the parents dir
+# first, create the parent directory
+
+
+revEx_12.parent.mkdir(parents=True,exist_ok=True)
+print(f"parent directory has been created.")
+
+# contents need to be written in the file needs to be a list of strings
+# with newlines.
+contents = ["Discovery\n","Enterprise\n","Defiant\n","Voyager"]
+
+if revEx_12.exists():
+    pass
+else:
+    with open(revEx_12,"w") as rv:
+        rv.writelines(contents)
+
+with open(revEx_12,"r") as rv:
+    text = rv.readlines()
+    for l in text:
+        print(l,end='')
+
+print()
+print()
+print("only print out startship starting with letter D")
+with open(revEx_12,"r") as rv:
+    text = rv.readlines()
+    for l in text:
+        if l[0] == 'D':
+            print(l,end='')
+
+
+
+print(f"""{sec}
+12.6 Read and Write CSV Data
+{sec}
+""")
+
+csv_12 = chapter12/"csv_12"
+csv_12.mkdir(parents = True,exist_ok=True)
+temperature_readings = [68, 65, 68, 70, 74, 72]
+temp_csv = csv_12/"temp_readings.csv"
+with open(temp_csv,"w") as csv:
+    csv.write(str(temperature_readings[0]))
+    for t in temperature_readings[1:]:
+        csv.write(f",{t}")
+
+with open(temp_csv,"r") as csv:
+    content = csv.read()
+    print(content)
+    sysout(content)
+
+temps = content.split(",")
+sysout(temps)
+
+# list comprehension.
+temps_int = [int(x) for x in temps]
+sysout(temps_int)
+
+
+print(f"""{seg}
+The csv Module
+1. Writing CSV Files With csv.writer
+2. Reading CSV Files With csv.reader
+{seg}
+""")
+
+import csv
+
+daily_temps = [[68, 65, 68, 70, 74, 72],[67, 67, 70, 72, 72, 70],[68, 70, 74, 76, 74, 73],]
+
+# create a csv file that you want to write data in.
+daily_temps_csv = csv_12/"daily_temps.csv"
+
+# without with statement
+
+# create a file object.
+file = daily_temps_csv.open(mode = "w",encoding="utf8")
+
+# create a new csv writer object
+writer = csv.writer(file)
+
+for row in daily_temps:
+    print(f"row is {row}")
+    print(f"type of row is {type(row)}")
+    writer.writerow(row)
+    print("row appended.")
+
+file.close()
+
+
+# use with statement:
+daily_temps_csv2 = csv_12/"daily_temps2.csv"
+with open(daily_temps_csv2,"w",encoding = "utf8") as f:
+    csv_writer = csv.writer(f)
+    csv_writer.writerows(daily_temps)
+
+print("csv_writer completed")
+print()
+with open(daily_temps_csv2,"r") as f:
+    content = f.readlines()
+    print(content)
+
+print(f"""{seg}
+The csv Module
+2. Reading CSV Files With csv.reader
+{seg}
+""")
+
+
+print(f"""{seg}
+The csv Module
+1. Writing CSV Files With csv.writer
+2. Reading CSV Files With csv.reader
+3.
+{seg}
 """)
